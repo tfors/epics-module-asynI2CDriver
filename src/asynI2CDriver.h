5a4cf8d6fdc9ebd1b736e75bd241f696a35b73e9
Thomas Fors <tom@fors.net>
2017-12-27 08:46:16 -0600

    Added code for asynI2CDriver

---
+++ src/asynI2CDriver.h
@@ -0,0 +1,28 @@
+/* asynI2CDriver.h
+ *
+ * EPICS asynPortDriver support for i2c devices under Linux
+ * using i2c-dev.
+ *
+ * Thomas Fors <tom@fors.net>
+ * December, 2017
+ */
+
+#include "asynPortDriver.h"
+
+class asynI2CDriver : public asynPortDriver {
+
+public:
+    asynI2CDriver(const char* portName, int i2cPortNum, int i2cAddr,
+                  int maxAddrIn, int interfaceMask, int interruptMask,
+                  int asynFlags, int autoConnect, int priority, int stackSize);
+
+protected:
+    int fd;
+    int i2cPortNum;
+    unsigned short i2cAddr;
+
+    asynStatus i2c_connect(asynUser* pasynUser);
+    asynStatus i2c_disconnect(asynUser* pasynUser);
+    int i2c_wr_rd(unsigned char* tx, unsigned short tx_n, unsigned char* rx,
+                  unsigned short rx_n);
+};
